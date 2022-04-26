4433e932427b13d08d1c25d61c485f30b225dff4
Bidisha Das <officialbidisha1@gmail.com>
2022-04-26 18:21:17 +0530

    Commmir2]

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json())
 
 app.listen(3000, () => {
     console.log(`Server Started at ${3000}`)
