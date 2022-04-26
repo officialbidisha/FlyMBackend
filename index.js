a51ad5a029228de2882a38030b25c73f38d8e019
Bidisha Das <officialbidisha1@gmail.com>
2022-04-26 17:14:19 +0530

    Added backend for cities

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
+})
+
+require('dotenv').config();
+
+const mongoString = 'mongodb+srv://officialbidisha1:Schatterjee5511$$SS@clusterflym.gamwr.mongodb.net/test'
+mongoose.connect(mongoString);
+const database = mongoose.connection
+
+const routes = require('./routes/routes');
+app.use('/api', routes)
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+
